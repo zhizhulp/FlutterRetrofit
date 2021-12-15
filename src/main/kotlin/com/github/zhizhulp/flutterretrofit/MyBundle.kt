1659e042f2f6245db55b4f14b901cb75d88043df
GitHub Action <action@github.com>
2021-12-15 03:53:53 +0000

    Template cleanup

--- src/main/kotlin/com/github/zhizhulp/flutterretrofit/MyBundle.kt
+++ src/main/kotlin/com/github/zhizhulp/flutterretrofit/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.zhizhulp.flutterretrofit
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/zhizhulp/flutterretrofit/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/zhizhulp/flutterretrofit/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.zhizhulp.flutterretrofit.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.zhizhulp.flutterretrofit.MyBundle
 
 class MyApplicationService {
 
